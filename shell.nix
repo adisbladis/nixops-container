c7c82248d863bfb9e4f815ee8cb49332edb99683
adisbladis <adisbladis@gmail.com>
2020-04-30 13:31:01 +0100

    Attempt to get SSH into github actions

--- shell.nix
+++ shell.nix
@@ -9,52 +9,13 @@ let
 
   pkgs = import nixpkgs {};
 
-  # To use this shell.nix on NixOS your user needs to be configured as such:
-  # users.extraUsers.adisbladis = {
-  #   subUidRanges = [{ startUid = 100000; count = 65536; }];
-  #   subGidRanges = [{ startGid = 100000; count = 65536; }];
-  # };
-
-  # Provides a script that copies required files to ~/
-  podmanSetupScript = let
-    registriesConf = pkgs.writeText "registries.conf" ''
-      [registries.search]
-      registries = ['docker.io']
-
-      [registries.block]
-      registries = []
-    '';
-  in pkgs.writeScript "podman-setup" ''
-    #!${pkgs.runtimeShell}
-
-    # Dont overwrite customised configuration
-    if ! test -f ~/.config/containers/policy.json; then
-      install -Dm555 ${pkgs.skopeo.src}/default-policy.json ~/.config/containers/policy.json
-    fi
-
-    if ! test -f ~/.config/containers/registries.conf; then
-      install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
-    fi
-  '';
-
 in pkgs.mkShell {
 
   buildInputs = [
-    pkgs.podman  # Docker compat
-    pkgs.runc  # Container runtime
-    pkgs.crun # Default container runtime (cgroups v2)
-    pkgs.conmon  # Container runtime monitor
-    pkgs.skopeo  # Interact with container registry
-    pkgs.slirp4netns  # User-mode networking for unprivileged namespaces
-    pkgs.fuse-overlayfs  # CoW for images, much faster than default vfs
-
     pkgs.nixops
+    pkgs.socat
   ];
 
-  shellHook = ''
-    # Install required configuration
-    export NIX_PATH=nixpkgs=${nixpkgs}
-    ${podmanSetupScript}
-  '';
+  NIX_PATH = "nixpkgs=${nixpkgs}";
 
 }
